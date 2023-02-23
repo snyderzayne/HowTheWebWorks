8aabd6009e63a0b79a92cb3265d111ade69f0e19
Zayne Snyder <snyderzayne@gmail.com>
2023-02-22 21:46:50 -0500

    giphy-party-complete

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -7,32 +7,32 @@ const removeAll = document.querySelector("#clear");
 search.addEventListener("click", function (e) {
   e.preventDefault();
   createImg();
+  input.value = "";
 });
 
 removeAll.addEventListener("click", function (e) {
   e.preventDefault();
   imageDiv.innerHTML = "";
+  input.value = "";
 });
 
 async function getGif(keyword) {
-  /*const res = await axios.get(
+  const res = await axios.get(
     `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=ay2JpZMux8eFE7kr9UYeEfOqlhTjzqHX`
-  );*/
-  axios.get(`http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=ay2JpZMux8eFE7kr9UYeEfOqlhTjzqHX`)
-  .then(res => console.log(res.data))
-  
-  //let resData = Array.from(res.data);
-  //console.log(res.data)
+  );
+  const resData = res.data;
+  console.log(resData)
   let randomNum = Math.floor(Math.random()*50)
   // console.log(res.data[1].images.original.url);
   // const { resData } = res.data;
-  //console.log(resData[randomNum].url); //return when it works
+  console.log(resData.data[randomNum])
+  return resData.data[randomNum].images.original.url; //return when it works
 }
 
 const imageDiv = document.querySelector("#images");
 
-function createImg() {
+async function createImg() {
   let newImg = document.createElement("img");
-  newImg.src = getGif(input.value);
+  newImg.src = await getGif(input.value);
   imageDiv.append(newImg);
 }
